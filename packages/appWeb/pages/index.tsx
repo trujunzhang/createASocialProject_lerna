1dae34c81219891089ccec81e969153b84a60c62
djzhang <trujunzhang@gmail.com>
2019-02-22 09:39:42 +0800

    primitives for web.

--- packages/appWeb/pages/index.tsx
+++ packages/appWeb/pages/index.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-primitives'
+import {
+  ColorListView
+} from '../src/components/lib/colorScheme/colorListView'
 
 const styles = StyleSheet.create({
   container: {
@@ -17,6 +20,7 @@ export default (props) => {
   return (
     <View style={styles.container} >
       <Text style={styles.text}> {'Welcome to Next.js with react - native - web, with typescript! '} </Text>
+      <ColorListView />
     </View>
   )
 }
