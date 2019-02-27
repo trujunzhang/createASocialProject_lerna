86a7dc2d403ba4e954c659588476b16c5f06de55
djzhang <trujunzhang@gmail.com>
2019-02-27 20:09:22 +0800

    Feather

--- packages/clientLibrary/NativeBase/native-base-variables/src/types/iVectorIcons.ts
+++ packages/clientLibrary/NativeBase/native-base-variables/src/types/iVectorIcons.ts
@@ -1,4 +1,21 @@
 
+export interface IVectorIcons {
+    AntDesign: string
+    Entypo: string
+    EvilIcons: string
+    Feather: string
+    FontAwesome: string
+    FontAwesome5: string
+    Foundation: string
+    Ionicons: string
+    MaterialCommunityIcons: string
+    MaterialIcons: string
+    Octicons: string
+    SimpleLineIcons: string
+    Zocial: string
+}
+
+
 export interface IIconVectorVariables {
-    iconRenderComponents: () => any
+    iconRenderComponents: () => IVectorIcons
 }
