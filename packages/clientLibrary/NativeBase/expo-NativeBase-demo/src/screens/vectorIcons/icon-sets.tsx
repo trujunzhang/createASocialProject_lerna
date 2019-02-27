0bcbcd4603ce7c8bf5263fd8d1abf020bddc0d3f
djzhang <trujunzhang@gmail.com>
2019-02-27 20:28:39 +0800

    MaterialCommunityIconsGlyphs

--- packages/clientLibrary/NativeBase/expo-NativeBase-demo/src/screens/vectorIcons/icon-sets.tsx
+++ packages/clientLibrary/NativeBase/expo-NativeBase-demo/src/screens/vectorIcons/icon-sets.tsx
@@ -1,10 +1,10 @@
 import { pipe, toPairs, groupBy, map } from 'ramda'
-// import AntD from 'react-native-vector-icons/AntDesign';
+import AntD from 'react-native-vector-icons/AntDesign';
 import Entypo from 'react-native-vector-icons/Entypo'
 import EvilIcons from 'react-native-vector-icons/EvilIcons'
 import Feather from 'react-native-vector-icons/Feather'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
-// import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Foundation from 'react-native-vector-icons/Foundation'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
@@ -13,12 +13,12 @@ import Octicons from 'react-native-vector-icons/Octicons'
 import Zocial from 'react-native-vector-icons/Zocial'
 
 import {
-  // AntDGlyphs,
+  AntDGlyphs,
   EntypoGlyphs,
   EvilIconsGlyphs,
   FeatherGlyphs,
   FontAwesomeGlyphs,
-  // FontAwesome5Glyphs,
+  FontAwesome5Glyphs,
   FoundationGlyphs,
   IoniconsGlyphs,
   MaterialIconsGlyphs,
@@ -28,12 +28,12 @@ import {
 } from './vectorIcons'
 
 const GLYPH_MAPS = {
-  // AntD: AntDGlyphs,
+  AntD: AntDGlyphs,
   Entypo: EntypoGlyphs,
   EvilIcons: EvilIconsGlyphs,
   Feather: FeatherGlyphs,
   FontAwesome: FontAwesomeGlyphs,
-  // FontAwesome5: FontAwesome5Glyphs,
+  FontAwesome5: FontAwesome5Glyphs,
   Foundation: FoundationGlyphs,
   Ionicons: IoniconsGlyphs,
   MaterialIcons: MaterialIconsGlyphs,
@@ -43,12 +43,12 @@ const GLYPH_MAPS = {
 }
 
 const ICON_SETS = {
-  // AntD,
+  AntD,
   Entypo,
   EvilIcons,
   Feather,
   FontAwesome,
-  // FontAwesome5,
+  FontAwesome5,
   Foundation,
   Ionicons,
   MaterialIcons,
--- packages/clientLibrary/NativeBase/expo-NativeBase-demo/src/screens/vectorIcons/vectorIcons.tsx
+++ packages/clientLibrary/NativeBase/expo-NativeBase-demo/src/screens/vectorIcons/vectorIcons.tsx
@@ -1,23 +1,23 @@
-// import * as AntDGlyphs from './glyphmaps/AntDesign.json';
-import * as EntypoGlyphs from './glyphmaps/Entypo.json'
-import * as EvilIconsGlyphs from './glyphmaps/EvilIcons.json'
-import * as FeatherGlyphs from './glyphmaps/Feather.json'
-import * as FontAwesomeGlyphs from './glyphmaps/FontAwesome.json'
-// import * as FontAwesome5Glyphs from './glyphmaps/FontAwesome5Free.json';
-import * as FoundationGlyphs from './glyphmaps/Foundation.json'
-import * as IoniconsGlyphs from './glyphmaps/Ionicons.json'
-import * as MaterialIconsGlyphs from './glyphmaps/MaterialIcons.json'
-import * as MaterialCommunityIconsGlyphs from './glyphmaps/MaterialCommunityIcons.json'
-import * as OcticonsGlyphs from './glyphmaps/Octicons.json'
-import * as ZocialGlyphs from './glyphmaps/Zocial.json'
+import * as AntDGlyphs from 'react-native-vector-icons/glyphmaps/AntDesign.json';
+import * as EntypoGlyphs from 'react-native-vector-icons/glyphmaps/Entypo.json'
+import * as EvilIconsGlyphs from 'react-native-vector-icons/glyphmaps/EvilIcons.json'
+import * as FeatherGlyphs from 'react-native-vector-icons/glyphmaps/Feather.json'
+import * as FontAwesomeGlyphs from 'react-native-vector-icons/glyphmaps/FontAwesome.json'
+import * as FontAwesome5Glyphs from 'react-native-vector-icons/glyphmaps/FontAwesome5Free.json';
+import * as FoundationGlyphs from 'react-native-vector-icons/glyphmaps/Foundation.json'
+import * as IoniconsGlyphs from 'react-native-vector-icons/glyphmaps/Ionicons.json'
+import * as MaterialIconsGlyphs from 'react-native-vector-icons/glyphmaps/MaterialIcons.json'
+import * as MaterialCommunityIconsGlyphs from 'react-native-vector-icons/glyphmaps/MaterialCommunityIcons.json'
+import * as OcticonsGlyphs from 'react-native-vector-icons/glyphmaps/Octicons.json'
+import * as ZocialGlyphs from 'react-native-vector-icons/glyphmaps/Zocial.json'
 
 export {
-  // AntDGlyphs,
+  AntDGlyphs,
   EntypoGlyphs,
   EvilIconsGlyphs,
   FeatherGlyphs,
   FontAwesomeGlyphs,
-  // FontAwesome5Glyphs,
+  FontAwesome5Glyphs,
   FoundationGlyphs,
   IoniconsGlyphs,
   MaterialIconsGlyphs,
