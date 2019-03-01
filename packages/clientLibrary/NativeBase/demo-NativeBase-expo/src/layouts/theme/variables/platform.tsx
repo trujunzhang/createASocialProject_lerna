5e83fc5be0fc025f1c16e980c41c6a92f97c3985
djzhang <trujunzhang@gmail.com>
2019-03-01 11:11:17 +0800

    StyleProvider

--- packages/clientLibrary/NativeBase/demo-NativeBase-expo/src/layouts/theme/variables/platform.tsx
+++ packages/clientLibrary/NativeBase/demo-NativeBase-expo/src/layouts/theme/variables/platform.tsx
@@ -1,10 +1,11 @@
 import { vectorIcons } from './vectorIcons'
 
-import { themeVariablesWithIconVector, appThemeVariables } from '@app/native-base-variables'
+import { themeVariablesWithIconVector, generatorForAppThemeVariables } from '@app/native-base-variables'
 
-export const platformVariables: themeVariablesWithIconVector = {
-  ...appThemeVariables,
-  iconRenderComponents: () => {
-    return vectorIcons
+export const platformVariables: themeVariablesWithIconVector = generatorForAppThemeVariables(
+  {
+    iconRenderComponents: () => {
+      return vectorIcons
+    }
   }
-}
+)
