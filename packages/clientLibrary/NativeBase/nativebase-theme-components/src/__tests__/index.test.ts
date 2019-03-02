5825ce131a93b1996f55c097cf3b1162fe8163a1
djzhang <trujunzhang@gmail.com>
2019-03-02 17:42:45 +0800

    primitives

---
+++ packages/clientLibrary/NativeBase/nativebase-theme-components/src/__tests__/index.test.ts
@@ -0,0 +1,18 @@
+import {
+    getThemeStyle
+} from '../index'
+
+import {
+    platformVariables,
+} from '@app/native-base-variables'
+
+
+describe('Verify the getThemeStyle', () => {
+    it('should return correctly', () => {
+        const result = getThemeStyle(platformVariables)
+
+        let x = 0
+
+        // expect(dependencies['tween-functions']).toBeDefined()
+    })
+})
