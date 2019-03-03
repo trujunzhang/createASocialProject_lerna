9dd225a7869f7774cbe12ec40b437cc454a6f4bd
djzhang <trujunzhang@gmail.com>
2019-03-03 14:56:10 +0800

    typescript

---
+++ packages/universalLibrary/react-primitives/src/injection/__tests__/react-sketchapp.test.ts
@@ -0,0 +1,13 @@
+// import { ReactPrimitives  } from '../../ReactPrimitives'
+
+import { allInjectionModel } from '../react-sketchapp'
+
+describe('Functions of the utils', () => {
+    test('should return string correctly,  invoked random', () => {
+        const Dimensions = allInjectionModel.Dimensions
+        const deviceHeight = Dimensions.get('window').height
+        const deviceWidth = Dimensions.get('window').width
+
+        expect(deviceWidth).toBe(40)
+    })
+})
