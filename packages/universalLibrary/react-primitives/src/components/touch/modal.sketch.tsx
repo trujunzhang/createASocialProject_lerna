3150899b7c81602e7daa775bf388dabb729295cc
djzhang <trujunzhang@gmail.com>
2019-03-04 15:46:44 +0800

    Modal

---
+++ packages/universalLibrary/react-primitives/src/components/touch/modal.sketch.tsx
@@ -0,0 +1,15 @@
+
+import {
+    FlatListProps
+} from '../../models/iTouchProps'
+
+import * as React from 'react'
+
+interface ISketchFlatlistState { }
+
+export class FlatList<ItemT> extends React.Component<FlatListProps<ItemT>, ISketchFlatlistState> {
+
+    render() {
+        return null
+    }
+}
---
+++ packages/universalLibrary/react-primitives/src/components/touch/modal.web.tsx
@@ -0,0 +1,5 @@
+import {
+    Modal
+} from 'react-native-web'
+
+export { Modal }
