17dcee973d7bc7ae0ec14079b650062ba6b90107
djzhang <trujunzhang@gmail.com>
2019-03-05 15:03:26 +0800

    postbuild

--- packages/appClient/babel.config.js
+++ packages/appClient/babel.config.js
@@ -32,11 +32,7 @@ module.exports = function (api) {
           ],
           "extensions": [".ios.js", ".android.js", ".js", ".jsx", ".json"],
           "alias": {
-            "^react-primitives$": "react-native",
-            "@src": "./src",
-            "@config": "./config",
-            "@assets": "./static",
-            "@client": "./apps/client"
+            "^react-primitives$": "react-native"
           }
         }
       ]
