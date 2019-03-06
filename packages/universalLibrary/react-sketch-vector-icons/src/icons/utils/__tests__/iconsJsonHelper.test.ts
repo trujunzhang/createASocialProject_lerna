bcc3a9ff7aefb780d7b9ae92a725db91bce6c065
djzhang <trujunzhang@gmail.com>
2019-03-06 21:18:24 +0800

    pause_circle_filled

--- packages/universalLibrary/react-sketch-vector-icons/src/icons/utils/__tests__/iconsJsonHelper.test.ts
+++ packages/universalLibrary/react-sketch-vector-icons/src/icons/utils/__tests__/iconsJsonHelper.test.ts
@@ -4,7 +4,7 @@ import { getSvgDataByIconName } from '../iconsJsonHelper'
 
 describe('methods correctly in the iconsJsonHelper', () => {
   test('should return string correctly, checking getSvgDataByIconName  ', () => {
-    const pathData: string | null = getSvgDataByIconName(svgJsonObject, 'MaterialIcons', 'arrow-back')
+    const pathData: string | null = getSvgDataByIconName(svgJsonObject, 'MaterialIcons', 'pause-circle-filled')
 
     expect(pathData).toBeDefined()
   })
