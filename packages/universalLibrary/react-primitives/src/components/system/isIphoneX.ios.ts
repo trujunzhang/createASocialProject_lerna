9837338ec859b70b878d5ed9fdc5c73dfa1b0524
djzhang <trujunzhang@gmail.com>
2019-03-07 21:20:52 +0800

    isIphoneX

--- packages/universalLibrary/react-primitives/src/components/system/isIphoneX.ios.ts
+++ packages/universalLibrary/react-primitives/src/components/system/isIphoneX.ios.ts
@@ -1,4 +1,13 @@
+import { Dimensions, Platform as RNPlatform } from 'react-native';
 
-export const isIphoneX = () => {
-    return true
+export function isIphoneX() {
+    const dimen = Dimensions.get('window');
+    const Platform: any = RNPlatform
+    return (
+        Platform.OS === 'ios' &&
+        !Platform.isPad &&
+        !Platform.isTVOS &&
+        ((dimen.height === 812 || dimen.width === 812) || (dimen.height === 896 || dimen.width === 896))
+    );
 }
+
