387a93abfaf0e602d1d28640da6890bc8396da59
djzhang <trujunzhang@gmail.com>
2019-03-07 20:24:17 +0800

    PixelRatio.get()

--- packages/universalLibrary/react-primitives/src/components/system/__tests__/PixelRatio.test.ts
+++ packages/universalLibrary/react-primitives/src/components/system/__tests__/PixelRatio.test.ts
@@ -1,10 +1,7 @@
-import { PixelRatioHelper } from '../PixelRatio'
-
-import { Dimensions } from '../../injection/sketchapp/dimensions'
+import { PixelRatio } from '../pixelRatio.sketch'
 
 describe('Functions of the PixelRatioHelper', () => {
   test('should return string correctly,  invoked scale', () => {
-    const PixelRatio = new PixelRatioHelper(Dimensions)
     const scale = PixelRatio.get()
 
     expect(scale).toBe(2)
