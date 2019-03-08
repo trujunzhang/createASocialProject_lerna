c16e3c956c5bb0cae2b1b6cb67c035afdb1ee062
djzhang <trujunzhang@gmail.com>
2019-03-08 21:33:35 +0800

    // issue for sketch

--- packages/clientLibrary/NativeBase/demo-NativeBase-SharedComps/src/screens/index.ts
+++ packages/clientLibrary/NativeBase/demo-NativeBase-SharedComps/src/screens/index.ts
@@ -1,12 +1,14 @@
+export * from './button'
+
 export * from './accordion'
 export * from './actionsheet'
 export * from './anatomy'
 export * from './badge'
-export * from './button'
-export * from './card'
 export * from './checkbox'
 export * from './datepicker'
-export * from './deckswiper'
 export * from './fab'
 export * from './footer'
 export * from './form'
+
+// export * from './card' // issue for sketch
+// export * from './deckswiper' // issue for sketch
--- packages/universalLibrary/react-primitives/src/components/touch/touchableOpacity.sketch.tsx
+++ packages/universalLibrary/react-primitives/src/components/touch/touchableOpacity.sketch.tsx
@@ -6,19 +6,19 @@ import { View, StyleSheet } from 'react-sketchapp'
 
 import { convertValidateStyle } from '../utils'
 
-interface ISketchTouchableOpacityState {}
+interface ISketchTouchableOpacityState { }
 
 export class TouchableOpacity extends React.Component<
   TouchableOpacityProps,
   ISketchTouchableOpacityState
-> {
+  > {
   render() {
     const { name, style } = this.props as any
 
     /* const compStyle = StyleSheet.flatten(style) */
     const compStyle = convertValidateStyle(style)
 
-    console.log('TouchableOpacity:', JSON.stringify(compStyle))
+    // console.log('TouchableOpacity:', JSON.stringify(compStyle))
 
     return (
       <View name={name} style={compStyle}>
