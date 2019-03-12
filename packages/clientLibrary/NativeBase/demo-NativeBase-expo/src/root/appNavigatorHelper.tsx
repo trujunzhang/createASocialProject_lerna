2d4c5a9a2ec1b094f786446598f27e00d0562c53
djzhang <trujunzhang@gmail.com>
2019-03-12 08:53:13 +0800

    navigator.

--- packages/clientLibrary/NativeBase/demo-NativeBase-expo/src/root/appNavigatorHelper.tsx
+++ packages/clientLibrary/NativeBase/demo-NativeBase-expo/src/root/appNavigatorHelper.tsx
@@ -5,98 +5,36 @@ import { stackRouteConfigs, stackConfig } from './drawerHelper'
 
 import { IconList } from '../screens/vectorIcons'
 import {
-  // Header
-  ListItemHeaderObject,
-    ListItemFooterObject ,
-    ListItemButtonObject ,
-    ListItemCardObject ,
-  // Segment
-  SegmentNB as AdvSegment,
-  // Deck
-  SimpleDeck,
-  AdvancedDeck,
-  // Picker
-  PickerWithIconStyle,
-  RegularPicker,
-  PickerWithIcon,
-  PlaceholderPicker,
-  PlaceholderPickerNote,
-  BackButtonPicker,
-  PickerTextItemText,
-  HeaderPicker,
-  HeaderStylePicker,
-  CustomHeaderPicker,
-  // ActionSheet
-  RegularActionSheet,
-  IconActionSheet,
-  // Icons
-  Icons,
-  BasicIcon,
-  StateIcon,
-  PlatformSpecificIcon,
-  IconFamily,
-    // Fab
-  BasicFab,
-  MultipleFab,
-  // Label
-  FixedLabel,
-  InlineLabel,
-  FloatingLabel,
-  PlaceholderLabel,
-  StackedLabel,
-  RegularInput,
-  UnderlineInput,
-  RoundedInput,
-  IconInput,
-  SuccessInput,
-  ErrorInput,
-  DisabledInput,
-  PickerInput,
-  // Swipe
-  BasicListSwipe,
-  SwipeRowCustomStyle,
-  MultiListSwipe,
-  // List
-  NHBasicList,
-  NHListItemSelected,
-  NHListDivider,
-  NHListSeparator,
-  NHListHeader,
-  NHListIcon,
-  NHListAvatar,
-  NHListThumbnail,
-  NHListItemNoIndent,
-  // Tab
-  BasicTab,
-  ConfigTab,
-  ScrollableTabExample,
-  SegmentHeader, // BasicSegment,
-  SegmentHeaderIcon,
-  // Radio
-  NHCustomRadio,
-  NHDefaultRadio,
-  // According
-  AccordionDefault,
-  AccordionIcon,
-  AccordionIconStyle,
-  AccordionHeaderContentStyle,
-  AccordionCustomHeaderContent,
-  // TextArea
-  TextAreaForm,
-  // Layout
-  RowNB,
-  ColumnNB,
-  NestedGrid,
-  CustomRow,
-  CustomCol,
-  // Toast
-  BasicToast,
-  ToastDuration,
-  ToastPosition,
-  ToastType,
-  ToastText,
-  ToastButton
-} from '@app/nb-demo-shared-components'
+    // Advanced
+    ListItemHeaderObject,
+    ListItemIconObject,
+    ListItemListObject,
+    ListItemListSwipeObject,
+    ListItemPickerObject,
+    ListItemRadioObject,
+    ListItemSearchBarObject,
+    ListItemSegmentObject,
+    ListItemSpinnerObject,
+    ListItemTabObject,
+    // Base
+    ListItemLayoutObject,
+    ListItemThumbnailObject,
+    ListItemToastObject,
+    ListItemTypographyObject,
+    // Screens
+    ListItemAccordionObject,
+    ListItemActionSheetObject,
+    ListItemAnatomyObject,
+    ListItemBadgeObject,
+    ListItemButtonObject,
+    ListItemCardObject,
+    ListItemCheckboxObject,
+    ListItemDatePickerObject,
+    ListItemDeckSwiperObject,
+    ListItemFabObject,
+    ListItemFooterObject,
+    ListItemFormObject,
+ } from '@app/nb-demo-shared-components'
 
 const DraftsStack = createStackNavigator(stackRouteConfigs, stackConfig)
 export const appRouteConfigs = {
