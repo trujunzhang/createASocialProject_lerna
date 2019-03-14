1b8db9f41fd8f37466d57f373a412f2acab4d04e
djzhang <trujunzhang@gmail.com>
2019-03-14 16:04:33 +0800

    rootDir

--- packages/universalLibrary/react-sketch-vector-icons/bin/buildOcticons.ts
+++ packages/universalLibrary/react-sketch-vector-icons/bin/buildOcticons.ts
@@ -22,7 +22,7 @@ import octicons from 'octicons'
 
 const SUFFIX = 'Icon'
 
-Object.getOwnPropertyNames(octicons).forEach(function(iconName) {
+Object.getOwnPropertyNames(octicons).forEach(function (iconName) {
   let { options, path: svgContents } = octicons[iconName]
   const { width, height, viewBox, class: className, 'aria-hidden': ariaHidden } = options
 
@@ -35,7 +35,7 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 
 const CLASS_NAME = '${className}'
-class ${componentName} extends React.Component {
+export class ${componentName} extends React.Component {
   render() {
     let defaults = {
       height: ${height},
@@ -66,23 +66,24 @@ class ${componentName} extends React.Component {
 ${componentName}.propTypes = {
   className: PropTypes.string
 }
-export default ${componentName}
 `
 
   const location = path.join(buildHelper.generatedIconPath, `${iconName}.tsx`)
 
   fs.writeFileSync(location, jsxSource)
+
+  const exportString = `export * from './icons/${iconName}'\r\n`.replace('\n\n', '\n')
+  fs.appendFileSync(buildHelper.mainTSPath, exportString, 'utf-8')
+
+  const exportTypeString = `export const ${componentName}: Icon\n`
+  fs.appendFileSync(buildHelper.mainTypingsPath, exportTypeString, 'utf-8')
 })
 
-var allComponents = Object.getOwnPropertyNames(octicons).map(function(iconName) {
-  const componentName = `${uppercamelcase(iconName)}${SUFFIX}`
-  return `
-import ${componentName} from './${iconName}'
-export {${componentName}}
-`
-})
+// var allComponents = Object.getOwnPropertyNames(octicons).map(function (iconName) {
+//   const exportString = `export * from './icons/${iconName}'\r`
+//   return exportString
+// })
 
-const indexSource = allComponents.join('\n')
+// const indexSource = allComponents.join('\n')
 
-fs.writeFileSync(buildHelper.mainTSPath, indexSource)
-a
+// fs.writeFileSync(buildHelper.mainTSPath, indexSource)
