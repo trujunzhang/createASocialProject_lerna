4e3e1a01565c4b6974be3546fede0ea16fa35006
djzhang <trujunzhang@gmail.com>
2019-03-15 17:20:58 +0800

    styled-icons

--- packages/clientLibrary/NativeBase/demo-NativeBase-sketch/src/layouts/boot/vectorIcons.ts
+++ packages/clientLibrary/NativeBase/demo-NativeBase-sketch/src/layouts/boot/vectorIcons.ts
@@ -14,6 +14,14 @@ import {
   Zocial
 } from '@app/react-sketch-vector-icons'
 
+// import * as crypto from 'styled-icons/crypto'
+// import * as Feather from 'styled-icons/feather'
+// import * as material from 'styled-icons/material'
+// import * as Octicons from 'styled-icons/octicons'
+// import * as typicons from 'styled-icons/typicons'
+
+// import { feather as Feather, octicons as Octicons } from 'styled-icons'
+
 import { IVectorIcons } from '@app/native-base-variables'
 export const vectorIcons: IVectorIcons = {
   AntDesign,
