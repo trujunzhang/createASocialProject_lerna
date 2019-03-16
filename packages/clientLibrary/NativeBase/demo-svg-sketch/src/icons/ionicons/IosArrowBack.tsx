bd28a5ec67f67279fb4d918ed954f997f88253a8
djzhang <trujunzhang@gmail.com>
2019-03-16 14:44:15 +0800

    IosArrowBack

--- packages/clientLibrary/NativeBase/demo-svg-sketch/src/icons/ionicons/IosArrowBack.tsx
+++ packages/clientLibrary/NativeBase/demo-svg-sketch/src/icons/ionicons/IosArrowBack.tsx
@@ -1,7 +1,7 @@
 
 import * as React from 'react';
 
-import { View, Text } from 'react-sketchapp'
+import { View, Text, StyleSheet } from 'react-sketchapp'
 import { Svg, render } from 'react-sketchapp';
 // import SvgUri from 'react-native-svg-uri';
 
@@ -16,9 +16,25 @@ import { Svg, render } from 'react-sketchapp';
 // }
 
 export const IosArrowBack = (props) => {
+  // const size = 512
+  const size = 280
   return (
-    <Svg height="512" width="512" viewBox="0 0 512 512">
-      <Svg.Path d="M217.9 256L345 129c9.4-9.4 9.4-24.6 0-33.9-9.4-9.4-24.6-9.3-34 0L167 239c-9.1 9.1-9.3 23.7-.7 33.1L310.9 417c4.7 4.7 10.9 7 17 7s12.3-2.3 17-7c9.4-9.4 9.4-24.6 0-33.9L217.9 256z" />
-    </Svg>
+    <View style={styles.container}>
+      <Svg
+        preserveAspectRatio="xMidYMid meet"
+        height={size} width={size} viewBox="0 0 512 512">
+        <Svg.Path d="M217.9 256L345 129c9.4-9.4 9.4-24.6 0-33.9-9.4-9.4-24.6-9.3-34 0L167 239c-9.1 9.1-9.3 23.7-.7 33.1L310.9 417c4.7 4.7 10.9 7 17 7s12.3-2.3 17-7c9.4-9.4 9.4-24.6 0-33.9L217.9 256z" />
+      </Svg>
+    </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+  },
+});
