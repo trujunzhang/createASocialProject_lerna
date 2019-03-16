4e25575df4986efd705822bd57b74782f624f68b
djzhang <trujunzhang@gmail.com>
2019-03-16 14:50:38 +0800

    preserveAspectRatio

--- packages/clientLibrary/NativeBase/demo-svg-sketch/src/icons/ionicons/IosArrowBack.tsx
+++ packages/clientLibrary/NativeBase/demo-svg-sketch/src/icons/ionicons/IosArrowBack.tsx
@@ -22,7 +22,7 @@ export const IosArrowBack = (props) => {
     <View style={styles.container}>
       <Svg
         preserveAspectRatio="xMidYMid meet"
-        height={size} width={size} viewBox="0 0 512 512">
+        height={size} width={280 * 57 / 100} viewBox="0 0 512 512">
         <Svg.Path d="M217.9 256L345 129c9.4-9.4 9.4-24.6 0-33.9-9.4-9.4-24.6-9.3-34 0L167 239c-9.1 9.1-9.3 23.7-.7 33.1L310.9 417c4.7 4.7 10.9 7 17 7s12.3-2.3 17-7c9.4-9.4 9.4-24.6 0-33.9L217.9 256z" />
       </Svg>
     </View>
